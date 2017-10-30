f005c0a1cff3d49bc79408708e312d2046ae6027
Alexis Contreras <aalexiscs@outlook.com>
2017-10-30 11:42:27 -0700

    I upload the solution

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Palindrome {
+    class Program {
+        static void Main(string[] args) {
+
+            string[] TextsEntered = { " ", " Anita lava la tina", " Maam ", "Ali tomo tila","This is not a palindrome", "Anna",
+                "Civic", "Kayak", "Level", "Madam", "Mom", "Noon", "Racecar", "Day", "kiss", "Alli ves sevilla", "Aman a panama", " " };
+
+            foreach(var item in TextsEntered) {
+
+                if (item.Trim().Length <= 0)
+                    continue;
+
+                if (ValidatePalindrome(string.Join("", item.Split(' ')).ToLower()))
+                    Console.WriteLine(item.Trim() + ", It is a Palindrome ");
+                else
+                    Console.WriteLine(item.Trim() + ", It is not a Palindrome");
+            }
+            Console.ReadKey();
+        }
+
+        private static bool ValidatePalindrome(string TextEntered) {
+            
+            var ArrayTextEntered = TextEntered.ToCharArray(0, TextEntered.Length);
+
+            for (int x = 0, y = ArrayTextEntered.Length -1; x < ArrayTextEntered.Length; x++, y--) {
+                if (ArrayTextEntered[x] != ArrayTextEntered[y])
+                    return false;
+            }
+            return true;
+        }
+
+    }
+}
